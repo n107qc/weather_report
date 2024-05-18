2270eff3cd729a2e9d5de28e249502cf2ee6ed1e
Бублик НАзарій <nazariy.bublyk09@gmail.com>
2024-05-18 12:30:40 +0300

    Змінили дизайн і добавили кнопки і карти

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from kivymd.uix.backdrop.backdrop import MDCard
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
+
+class Weather(MDCard):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 class Mainscreen(MDScreen):
     def __init__(self, *args, **kwargs):
@@ -11,6 +16,7 @@ class MainApp(MDApp):
     def build(self):
         Builder.load_file('style.kv')
         self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Yellow"
         self.screen = Mainscreen("main_screen")
         return self.screen
 
