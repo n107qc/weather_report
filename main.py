ed94097ba76d3844df692862dccc3d9d4a227916
Бублик НАзарій <nazariy.bublyk09@gmail.com>
2024-06-01 12:52:11 +0300

    Показує Погоду

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.uix.settings import text_type
 from kivymd.uix.backdrop.backdrop import MDCard
 from kivy.lang import Builder
 from kivymd.app import MDApp
@@ -10,8 +11,13 @@ from config import API_KEY, API_URL
 API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=uk"
 
 class Weather(MDCard):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, description,icon,temp,rain,wind ,*args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.ids.desc_text.text = description
+        self.ids.temp_text.text = f'{temp}°C'
+        self.ids.rain_text.text = f'Ймовірність опадів: {rain*100}%'
+        self.ids.wind_text.text = f'Швидкість вітру: {wind}м/c'
+        self.ids.weather_icon.source = f"https://openweathermap.org/img/wn/{icon}@2x.png"
 
 class Mainscreen(MDScreen):
     def __init__(self, *args, **kwargs):
@@ -22,11 +28,25 @@ class Mainscreen(MDScreen):
             'q':city,
             "appid":API_KEY
         }
-        inf = requests.get(API_URL,api_params)
-        response = inf.json()
+        
+        
+        data = requests.get(API_URL, api_params)
+        response = data.json()
         print(response)
-        deck_weather = response['weather'][0]['deck_weather']
-        self.ids.weather_card.ids.label.text = deck_weather
+        description = response['weather'][0]['description']
+        icon = response['weather'][0]['icon']
+        temp = response['main']['temp']
+        if 'rain' in response:
+            rain = response['rain']['1h']
+        else:
+            rain = 0
+        wind = response['wind']['speed']
+        new_card = Weather(description,icon,temp,rain,wind)
+        self.ids.weather_carousel.add_widget(new_card)
+
+
+
+
 
 class MainApp(MDApp):
     def build(self):
