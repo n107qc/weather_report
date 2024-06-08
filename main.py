4f14b51830f7688a3c047d28ba606c13475f2aa3
Бублик НАзарій <nazariy.bublyk09@gmail.com>
2024-06-08 12:45:00 +0300

    Має бути як в всіх

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from kivymd.uix.pickers.datepicker.datepicker import date
+from kivy.uix.screenmanager import NoTransition
 from kivy.uix.settings import text_type
 from kivymd.uix.backdrop.backdrop import MDCard
 from kivy.lang import Builder
@@ -7,12 +9,13 @@ from kivymd.uix.screen import MDScreen
 from config import API_KEY
 import requests
 
-from config import API_KEY, API_URL
+from config import API_KEY, API_URL, FORECAST_URL
 API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=uk"
 
 class Weather(MDCard):
-    def __init__(self, description,icon,temp,rain,wind ,*args, **kwargs):
+    def __init__(self, date_time, description,icon,temp,rain,wind ,*args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.ids.date_text.text = date_time
         self.ids.desc_text.text = description
         self.ids.temp_text.text = f'{temp}°C'
         self.ids.rain_text.text = f'Ймовірність опадів: {rain*100}%'
@@ -22,27 +25,53 @@ class Weather(MDCard):
 class Mainscreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-    def weather_search(self):
-        city = self.ids.city_field.text.strip().lower()
+    def get_weather_data(self,url,city):
         api_params = {
             'q':city,
             "appid":API_KEY
         }
         
         
-        data = requests.get(API_URL, api_params)
-        response = data.json()
-        print(response)
+        data = requests.get(url, api_params)
+        if data.status_code == 200:
+            response = data.json()
+            return response
+        else:
+            return None
+        
+    
+    def add_weather_card(self,response):
         description = response['weather'][0]['description']
         icon = response['weather'][0]['icon']
         temp = response['main']['temp']
         if 'rain' in response:
-            rain = response['rain']['1h']
+            if  '1h' in response['rain']:
+                rain = response['rain']['1h']
+            else:
+                rain = response['rain']['3h']
         else:
             rain = 0
         wind = response['wind']['speed']
-        new_card = Weather(description,icon,temp,rain,wind)
+        if 'dt_txt' in response:
+            date_time =  response['dt_txt']
+        else:
+            date_time = 'Зараз'
+        new_card = Weather(date_time,description,icon,temp,rain,wind)
         self.ids.weather_carousel.add_widget(new_card)
+
+    def weather_search(self):
+        self.ids.weather_carousel.clear_widgets()
+        city = self.ids.city_field.text.strip().lower()
+        
+        current_weather = self.get_weather_data(API_URL,city)
+        forecast = self.get_weather_data(FORECAST_URL,city)
+        
+        if current_weather:
+            self.add_weather_card(current_weather)
+
+        if forecast:
+            for period in forecast['list']:
+                self.add_weather_card(period)
 
 
 
